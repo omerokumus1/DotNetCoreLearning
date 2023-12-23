eb5b8cbf6064b4c90444e68b4d69c1fc7be3e1f3
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-23 13:28:12 +0300

    TempDataProvider, SessionStateProvider

--- DotNetCoreLearning/Controllers/HomeController.cs
+++ DotNetCoreLearning/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        if (TempData["Country"] != null)
+        {
+            TempData["Country"] = "Turkey";
+        }
+        
         return View();
     }
 
--- DotNetCoreLearning/Program.cs
+++ DotNetCoreLearning/Program.cs
@@ -1,7 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddControllersWithViews()
+    .AddSessionStateTempDataProvider();
+
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -19,6 +23,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
